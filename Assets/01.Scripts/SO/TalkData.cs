8e44178826f5266965b8b864455905cb444d4682
nacfson <nacfson@gmail.com>
2023-07-03 15:03:52 +0900

    dd

---
+++ Assets/01.Scripts/SO/TalkData.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "SO/Talk",fileName = "TalkData")]
+public class TalkData : ScriptableObject {
+    //마지막에 쓰여지는 string 값이 먼저 나옴
+    public List<string> talkList = new List<string>();
+    public bool CanGetTalk(int returnIdx) {
+        //return talkList[returnIdx] != null;
+        return true;
+    }
+    public string GetTalk(int returnIdx) {
+        string returnValue = talkList[returnIdx];
+        return returnValue;
+    }
+
+}
--- Assets/01.Scripts/UIToolkit/Main/UT_MainUI.cs
+++ Assets/01.Scripts/UIToolkit/Main/UT_MainUI.cs
@@ -3,42 +3,103 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 using DG.Tweening;
+using System;
 
 namespace UI_Toolkit{
     public class UT_MainUI : MonoBehaviour {
         private UIDocument _document;
-
         private VisualElement _root;
-        private VisualElement _infoUI;
+        private InfoUI _infoUI;
 
+        public static UT_MainUI Instance;
+
+        [SerializeField] private TalkData _talkData;
+        private void Awake() {
+            if(Instance == null) {
+                Instance = this;
+                Debug.LogError(Instance);
+            }
+        }
         private void OnEnable() {
             _document = GetComponent<UIDocument>();
 
             _root = _document.rootVisualElement;
-            _infoUI = _root.Q<VisualElement>("InfoUI");
+
+            VisualElement iu = _root.Q<VisualElement>("InfoUI");
+
+            _infoUI = new InfoUI(iu,_talkData,() => Debug.Log("OnInfoUIClicked"));
         }
 
 
         private void Update() {
             if (Input.GetKeyDown(KeyCode.T)) {
-                SetInfoUI("I Found The Rocket");
+                _infoUI.ShowText();
+            }
+        }
+    }
+
+
+    public class InfoUI {
+        //실질적 InfoUI의 VisualElement
+        private VisualElement _infoUI;
+        //InfoUI의 Label(text)
+        private Label _contentLabel;
+        private TalkData _talkData;
+        private bool _isAnimating = false;
+        private int _returnIdx;
+
+        public InfoUI(VisualElement infoUI,TalkData talkData, Action action) {
+            this._infoUI = infoUI;
+            this._talkData = talkData;
+            _contentLabel = infoUI.Q<Label>("ContentLabel");
+            _returnIdx = talkData.talkList.Count - 1;
+
+            RegisterAction(action);
+        }
+        public void RegisterAction(Action action) {
+            _infoUI.RegisterCallback<PointerDownEvent>(e => action());
+        }
+        public void ShowText() {
+            if (_isAnimating) {
+                _contentLabel.text = _contentLabel.tooltip;
+                _isAnimating = false;
+            }
+            else {
+                if (_talkData.CanGetTalk(_returnIdx)) {
+                    Debug.LogError("CanTalk");
+                    UI_Toolkit.UT_MainUI.Instance.StartCoroutine(AnimateTextCor(_talkData.GetTalk(_returnIdx),0.5f));
+                    _returnIdx--;
+                }
             }
         }
 
-        public void SetInfoUI(string content) {
-            Label con = _infoUI.Q<Label>("ContentLabel");
+        //startDelay => Transition Animation이 끝날때까지 기다려주는 시간
+        //typingDelay => 글자가 입력되는 속도
+        public IEnumerator AnimateTextCor(string text, float startDelay, float typingDelay = 0.1f) {
+            _contentLabel.text = string.Empty;
+            _contentLabel.tooltip = text;
             _infoUI.AddToClassList("active");
 
-            StartCoroutine(AnimateTextCor(content, con, 0.5f));
-        }
-        //startDelay => Transition Animation이 끝날때까지 기다려주는 시간
-        //typingDelay => 글자가 입력되는 속도ㄴ
-        private IEnumerator AnimateTextCor(string text,Label label,float startDelay,float typingDelay = 0.08f) {
+            _isAnimating = true;
+
             yield return new WaitForSeconds(startDelay);
-            for(int i = 0; i <= text.Length; i++) {
-                label.text = text.Substring(0,i);
-                yield return new WaitForSeconds(typingDelay);
+            int i = 0;
+            while (_contentLabel.text.Length != text.Length) {
+                if (!_isAnimating) yield break;
+
+                float timer = 0f;
+
+                while (timer < typingDelay) {
+                    //여기서도 yield break;를 할진느 고민해야됨
+                    timer += Time.deltaTime;
+                    yield return null;
+                }
+                _contentLabel.text = text.Substring(0, i);
+                i++;
+                yield return null;
             }
+            _contentLabel.text = text.Substring(0, text.Length);
+            _isAnimating = false;
         }
     }
 }
